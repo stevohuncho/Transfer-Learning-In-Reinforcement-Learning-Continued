951529f405e4ee0ded209b5d8bfe3536f70d39cb
stevo <spolin44@gmail.com>
2023-09-07 03:14:49 -0400

    added dqn goal transfer

---
+++ constants.py
@@ -0,0 +1,23 @@
+RENDER_MODE = "rgb_array"
+FPS = 1000
+EVAL_EPS = 200
+
+TOP_RIGHT = [
+    'SFFG', 
+    'FFHF', 
+    'FHFF', 
+    'FFFF'
+]
+BOTTOM_LEFT = [
+    'SFFF', 
+    'FFHF', 
+    'FHFF', 
+    'GFFF'
+
+]
+BOTTOM_RIGHT = [
+    'SFFF', 
+    'FFHF', 
+    'FHFF', 
+    'FFFG'
+]
